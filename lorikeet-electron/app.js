62576e1e8b3875548a59d33ea5231dd7d3047366
Jesse <ab9421@126.com>
2020-10-17 21:31:35 +0800

    ✨feat(lorikeet-electron):
    判断是否为文件或者文件夹

--- lorikeet-electron/app.js
+++ lorikeet-electron/app.js
@@ -2,6 +2,8 @@
 
 const osenv = require('osenv');
 const fs = require('fs');
+const async = require('async');
+const path = require('path');
 
 function getUserHomeFolder() {
     return osenv.home();
@@ -11,15 +13,51 @@ function getFilesInFolder(folderPath, cb) {
     fs.readdir(folderPath, cb);
 }
 
+function inspectAndDescribeFile(filePath, cb) {
+    let result = {
+        file: path.basename(filePath),
+        path: filePath, type: ''
+    }
+
+    fs.stat(filePath, (err, stat) => {
+        if (err) {
+            cb(err)
+        } else {
+            if (stat.isFile()) {
+                result.type = 'file';
+            }
+            if (stat.isDirectory()) {
+                result.type = 'directory'
+            }
+            cb(err, result)
+        }
+    });
+}
+
+// 使用async模块调用异步函数并收集结果
+function inspectAndDescribeFiles(folderPath, files, cb) {
+    async.map(files, (file, asyncCb) => {
+        let resolvedFilePath = path.resolve(folderPath, file);
+        inspectAndDescribeFile(resolvedFilePath, asyncCb);
+    }, cb)
+}
+
+// 显示文件列表信息
+function displayFiles(err, files) {
+    if (err) {
+        return alert('Sorry, we could not display your files');
+    }
+    files.forEach((file) => {
+        console.log(file,)
+    })
+}
 function main() {
-    const folderPath = getUserHomeFolder();
+    let folderPath = getUserHomeFolder();
     getFilesInFolder(folderPath, (err, files) => {
         if (err) {
             return alert('Sorry, we could not load your home folder')
         }
-        files.forEach((file) => {
-            console.log(`${folderPath}/${file}`)
-        })
+        inspectAndDescribeFiles(folderPath, files, displayFiles);
     })
 }
 
